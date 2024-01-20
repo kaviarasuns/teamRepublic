5a522acabc551f22d5a6f558f53ad60b291160fe
kaviarasuns <kaviarasu_ns@icloud.com>
2024-01-20 18:39:08 +0530

    added modules

--- backend-nestjs/src/app.module.ts
+++ backend-nestjs/src/app.module.ts
@@ -1,9 +1,14 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { StateModule } from './state/state.module';
+import { ConstituencyModule } from './constituency/constituency.module';
+import { PartyModule } from './party/party.module';
+import { CandidateModule } from './candidate/candidate.module';
+import { ElectionResultsModule } from './election-results/election-results.module';
 
 @Module({
-  imports: [],
+  imports: [StateModule, ConstituencyModule, PartyModule, CandidateModule, ElectionResultsModule],
   controllers: [AppController],
   providers: [AppService],
 })
